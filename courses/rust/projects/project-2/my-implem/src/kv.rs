b51447a481228c58f63d28b6313377011ae417d8
grouville <guillaume.derouville@gmail.com>
2023-04-23 23:31:28 +0200

    part2: fix offset, still an issue on tests + part 7
    Signed-off-by: grouville <guillaume.derouville@gmail.com>

--- courses/rust/projects/project-2/my-implem/src/kv.rs
+++ courses/rust/projects/project-2/my-implem/src/kv.rs
@@ -2,12 +2,14 @@ use crate::error::{MyError, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{BufReader, BufWriter, Seek, SeekFrom, Write};
 use std::path::Path;
 
 pub struct KvStore {
-    store: HashMap<String, String>,
-    file: BufWriter<File>,
+    pub store: HashMap<String, u64>,
+    writer: BufWriter<File>,
+    reader: BufWriter<File>,
+    offset: u64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -29,22 +31,34 @@ impl KvStore {
             .read(true)
             .open(&log_path)?;
 
-        let reader = BufReader::new(&log_file);
-        for line in reader.lines() {
-            let cmd: Command = serde_json::from_str(&line?)?;
+        let mut reader = BufReader::new(&log_file);
+        let mut offset = reader.seek(SeekFrom::Start(0))?;
+
+        let mut deserializer = serde_json::Deserializer::from_reader(&mut reader);
+        while let Some(cmd) = Command::deserialize(&mut deserializer).ok() {
+            let cmd_bytes = serde_json::to_vec(&cmd)?;
+
             match cmd {
-                Command::Set { key, value } => {
-                    store.insert(key, value);
+                Command::Set { key, value: _ } => {
+                    store.insert(key, offset);
                 }
                 Command::Remove { key } => {
                     store.remove(&key);
                 }
             }
+            offset += cmd_bytes.len() as u64;
         }
+        // println!("Offset: {:?}", store);
 
-        let file = BufWriter::new(log_file.try_clone()?);
+        let writer = BufWriter::new(log_file.try_clone()?);
+        let reader = BufWriter::new(log_file.try_clone()?);
 
-        Ok(KvStore { store, file })
+        Ok(KvStore {
+            store,
+            writer,
+            reader,
+            offset,
+        })
     }
 
     pub fn new() -> KvStore {
@@ -55,35 +69,58 @@ impl KvStore {
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let cmd = Command::Set {
             key: key.clone(),
-            value: value.clone(),
+            value,
         };
-        let cmd_str = serde_json::to_string(&cmd)?;
+        let offset = self.offset;
 
-        // Flush and sync the file before writing to it
-        self.file.flush()?;
-        self.file.get_ref().sync_all()?;
+        // Write the command to the file
+        serde_json::to_writer(&mut self.writer, &cmd)?;
+        self.writer.flush()?;
 
-        writeln!(self.file, "{}", cmd_str)?;
+        let cmd_bytes = serde_json::to_vec(&cmd)?;
 
-        self.store.insert(key, value);
+        // insert in the store
+        self.store.insert(key, offset + cmd_bytes.len() as u64);
+
+        // Get the len of the serialized command
+        // println!("Offset_set: {}", self.offset);
         Ok(())
     }
 
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
-        Ok(self.store.get(&key).cloned())
+        // println!("Offset_get_crap: {:?}", self.store);
+        match self.store.get(&key) {
+            Some(offset) => {
+                // println!("Offset: {}", offset);
+
+                let mut reader = BufReader::new(self.reader.get_ref());
+                reader.seek(SeekFrom::Start(offset.clone()))?;
+                let mut deserializer = serde_json::Deserializer::from_reader(reader);
+
+                let cmd_result = Command::deserialize(&mut deserializer);
+
+                match cmd_result {
+                    Ok(cmd) => match cmd {
+                        Command::Set { key: _, value } => Ok(Some(value)),
+                        Command::Remove { key: _ } => Ok(None),
+                    },
+                    Err(_) => Err(MyError::KeyNotFound),
+                }
+            }
+            None => Ok(None),
+        }
     }
 
     pub fn remove(&mut self, key: String) -> Result<()> {
         if self.store.remove(&key).is_some() {
             let cmd = Command::Remove { key: key.clone() };
-            let cmd_str = serde_json::to_string(&cmd)?;
 
-            // Flush and sync the file before writing to it
-            self.file.flush()?;
-            self.file.get_ref().sync_all()?;
+            serde_json::to_writer(&mut self.writer, &cmd)?;
+            self.writer.flush()?;
 
-            writeln!(self.file, "{}", cmd_str)?;
-
+            // Get the len of the serialized command
+            let cmd_bytes = serde_json::to_vec(&cmd)?;
+            self.offset += cmd_bytes.len() as u64; // Update the offset
             Ok(())
         } else {
             Err(MyError::KeyNotFound)
--- courses/rust/projects/project-2/my-implem/tests/tests.rs
+++ courses/rust/projects/project-2/my-implem/tests/tests.rs
@@ -212,6 +212,7 @@ fn overwrite_value() -> Result<()> {
     // Open from disk again and check persistent data.
     drop(store);
     let mut store = KvStore::open(temp_dir.path())?;
+    eprintln!("STORE: {:?}", store.store);
     assert_eq!(store.get("key1".to_owned())?, Some("value2".to_owned()));
     store.set("key1".to_owned(), "value3".to_owned())?;
     assert_eq!(store.get("key1".to_owned())?, Some("value3".to_owned()));
